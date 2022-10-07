530ea0b3e46e0e750529e7430eb1648f8a1fea01
Sorokin0 <s.aleksander.99.09@gmail.com>
2022-10-07 19:47:12 +0300

    Week 5

---
+++ ANDRangers/Program.cs
@@ -0,0 +1,31 @@
+﻿// Temp <= 0 - Freezing cold
+// Temps > 0 AND <= 10 - Cold
+// Temps >= 10 AND <= 15 - Chilly
+// Temps >= 15 AND < 20 - Warm
+// Temps >= 20 AND < 30 - Hot
+// Temp >= 30 - Boiling hot
+
+Console.WriteLine("Enter the temperature:");
+int temp = Int32.Parse(Console.ReadLine());
+
+    if (temp >= 30)
+    {
+        Console.WriteLine("Boiling hot");
+    }
+    else if (temp < 30 && temp >= 20)
+    {
+        Console.WriteLine("Hot");
+    }
+    else if (temp < 20 && temp >= 15)
+    {
+        Console.WriteLine("Warm");
+    } else if (temp < 15 && temp >= 10)
+    {
+        Console.WriteLine("Chilly");
+    } else if (temp < 10 && temp >= 0)
+    {
+        Console.WriteLine("Cold");
+    } else
+    {
+        Console.WriteLine("Freezing cold");
+    }
---
+++ OROperator/Program.cs
@@ -0,0 +1,21 @@
+﻿// Math >= 90 OR Chemistry >=90
+
+int math, chemistry,biology;
+
+Console.WriteLine("Enter your Math result:");
+math = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter your Biology result:");
+biology = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter your Chemistry results:");
+chemistry = Int32.Parse(Console.ReadLine());
+
+if((math >= 90 && chemistry >= 90) || (biology >= 90 && chemistry >= 90) || (biology >= 90 && math >= 90))
+{
+    Console.WriteLine("Congratulations! You will got accepted");
+}
+else
+{
+    Console.WriteLine("Your application cannot be approved");
+}
